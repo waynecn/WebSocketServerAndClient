1c37e61a4d352bd4d992aadc74eabd7a872b2573
zhangsan <zhangsan@test.com>
2020-11-17 16:57:51 +0800

    tabWidget的标签样式更改

--- EasyChat/chatwidget.cpp
+++ EasyChat/chatwidget.cpp
@@ -55,6 +55,30 @@ ChatWidget::ChatWidget(QWidget *parent) :
     for (int i = 0; i < nCount; ++i) {
         ui->showMsgTabWidget->removeTab(0);
     }
+    ui->showMsgTabWidget->setStyleSheet("QTabWidget::pane { border-top: 2px solid #C2C7CB;  }\
+                                        QTabWidget::tab-bar {left: 5px; }\
+                                        QTabBar::tab {      background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+                                                                        stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,\
+                                                                        stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);\
+                                            border: 2px solid #C4C4C3;\
+                                            border-bottom-color: #C2C7CB; \
+                                            border-top-left-radius: 4px;\
+                                            border-top-right-radius: 4px;\
+                                            min-width: 8ex;\
+                                            padding: 2px;\
+                                        }\
+                                        QTabBar::tab:selected, QTabBar::tab:hover {\
+                                            background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+                                                                        stop: 0 #fafafa, stop: 0.4 #f4f4f4,\
+                                                                        stop: 0.5 #e7e7e7, stop: 1.0 #fafafa);\
+                                        }\
+                                        QTabBar::tab:selected {\
+                                            border-color: #9B9B9B;\
+                                            border-bottom-color: #C2C7CB;\
+                                        }\
+                                        QTabBar::tab:!selected {\
+                                            margin-top: 2px; \
+                                            }");
     ui->showMsgTabWidget->addTab(m_pTextBrowser, "主窗口");
     ui->showMsgTabWidget->setTabsClosable(true);
     ui->showMsgTabWidget->tabBar()->setTabButton(0, QTabBar::RightSide, nullptr);
@@ -417,7 +441,6 @@ void ChatWidget::OnItemDoubleClicked(QTableWidgetItem *item) {
     }
 
     int nRow = item->row();
-    qDebug() << "nRow:" << nRow;
     QString userId = m_vecOnlineUsers[nRow].strUserId;
     for (int i = 0; i < m_vecUserIds.size(); ++i) {
         if (userId == m_vecUserIds[i]) {
