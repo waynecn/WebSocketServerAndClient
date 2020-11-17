85b55313bf2ce5af40ab6c38bc5155ef17e2083e
codears <wylworkmail@gmail.com>
2020-11-17 17:54:55 +0800

    进度条时间显示时分秒

--- EasyChat/progressdialog.cpp
+++ EasyChat/progressdialog.cpp
@@ -26,7 +26,20 @@ void ProgressDialog::SetProgress(qint64 val, qint64 total) {
 
 void ProgressDialog::SetLeftTime(qint64 timeLeft) {
     int leftSec = timeLeft / 1000;
-    ui->leftTime->setText(QString("%1秒").arg(leftSec));
+    int leftHour = 0;
+    int leftMiniute = 0;
+    QString str = "";
+    if (leftSec > 3600) {
+        leftHour = leftSec / 3600;
+        str = QString("%1小时%2分钟%3秒").arg(leftHour).arg((leftSec - 3600) / 60).arg(leftSec % 60);
+    } else if (leftSec <= 3600 && leftSec > 60) {
+        leftMiniute = leftSec / 60;
+        str = QString("%1分钟%2秒").arg(leftMiniute).arg(leftSec % 60);
+    } else {
+        str = QString("%1秒").arg(leftSec);
+    }
+
+    ui->leftTime->setText(str);
 }
 
 void ProgressDialog::on_okPushButton_clicked()
