a5570e49c0bde3dadc926b3b2144d8c488189bde
zhangsan <zhangsan@test.com>
2020-11-18 18:39:38 +0800

    修改按钮样式

--- EasyChat/chatwidget.cpp
+++ EasyChat/chatwidget.cpp
@@ -21,6 +21,48 @@ ChatWidget::ChatWidget(QWidget *parent) :
 {
     m_bIsMainWindow = true;
     ui->setupUi(this);
+    ui->uploadFilePushButton->setStyleSheet("QPushButton { border: 2px solid #8f8f91; border-radius: 6px; \
+                                            background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, \
+                                            stop: 0 #f6f7fa, stop: 1 #dadbde); \
+                                            min-width: 60px; } \
+                                        QPushButton:pressed { \
+                                            background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, \
+                                            stop: 0 #dadbde, stop: 1 #f6f7fa); \
+                                        } \
+                                        QPushButton:flat { \
+                                            border: none; \
+                                        } \
+                                        QPushButton:default { \
+                                            border-color: navy; \
+                                        }");
+    ui->fileListPushButton->setStyleSheet("QPushButton { border: 2px solid #8f8f91; border-radius: 6px; \
+                                          background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, \
+                                          stop: 0 #f6f7fa, stop: 1 #dadbde); \
+                                          min-width: 75px; } \
+                                      QPushButton:pressed { \
+                                          background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, \
+                                          stop: 0 #dadbde, stop: 1 #f6f7fa); \
+                                      } \
+                                      QPushButton:flat { \
+                                          border: none; \
+                                      } \
+                                      QPushButton:default { \
+                                          border-color: navy; \
+                                      }");
+    ui->sendMsgPushButton->setStyleSheet("QPushButton { border: 2px solid #8f8f91; border-radius: 6px; \
+                                         background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, \
+                                         stop: 0 #33A1C9, stop: 1 #dadbde); \
+                                         min-width: 75px; } \
+                                     QPushButton:pressed { \
+                                         background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, \
+                                         stop: 0 #33A1C9, stop: 1 #4169E1); \
+                                     } \
+                                     QPushButton:flat { \
+                                         border: none; \
+                                     } \
+                                     QPushButton:default { \
+                                         border-color: navy; \
+                                     }");
 
     m_strImageDir = APPLICATION_IMAGE_DIR;
     //图片缓存目录不存在则创建
